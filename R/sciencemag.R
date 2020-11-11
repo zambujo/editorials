1c2f83d9ca5a78aa0791e8d6a7d42ac2897d8bb3
Martins <zambuj@gmail.com>
2020-11-11 17:03:04 +0100

    ✨  Added a few good manners (Fixes #1)

--- R/sciencemag.R
+++ R/sciencemag.R
@@ -3,32 +3,33 @@ source(here("R", "packages.R"))
 
 # logic -------------------------------------------------------------------
 
-parse_year_issues <- function(year,
-                              base_url = "https://science.sciencemag.org") {
-  current_url <- glue("{base_url}/content/by/year/{year}")
-  glue("Parsing {current_url} ...") %>% message()
-
-  url_list <- current_url %>%
-    read_html() %>%
+parse_year_issues <- function(year, polite_bow) {
+  glue("Parsing /content/by/year/{year} ...") %>% message()
+  session <- nod(bow = polite_bow,
+                 path = paste0("content/by/year/", year))
+  scrape(session) %>%
     html_nodes(".issue-month-detail .highwire-cite-linked-title") %>%
     html_attr("href")
-
-  glue("{base_url}{url_list}")
 }
 
-parse_twil <- function(twil_url) {
+parse_twil <- function(twil_url, polite_bow) {
   glue("Parsing {twil_url} ...") %>% message()
+  session <- nod(bow = polite_bow,
+                 path = twil_url)
+  twil_html <- scrape(session)
 
-  twil_html <- twil_url %>% read_html()
+  ref_title <- twil_html %>%
+    html_nodes("#content-block-markup h1") %>%
+    html_text()
+
+  ref_editor <- twil_html %>%
+    html_nodes(".name") %>%
+    html_text()
 
   ref_topics <- twil_html %>%
     html_nodes(".compilation-overline") %>%
     html_text() %>%
     str_to_title()
-
-  ref_title <- twil_html %>%
-    html_nodes("#content-block-markup h1") %>%
-    html_text()
 
   twil_reference <- twil_html %>%
     html_nodes("ol .compilation")
@@ -49,38 +50,40 @@ parse_twil <- function(twil_url) {
     html_nodes(p_path) %>%
     html_text()
 
-  res <- tibble(
+  if (length(ref_editor) == 0)
+    ref_editor = rep(NA_character_, length(ref_title))
+
+  tibble(
     title = ref_title,
+    editor = ref_editor,
     topic = ref_topics,
     url = ref_url,
     ref = ref_paper
   )
-
-  res %>%
-    mutate(
-      ref = str_split(ref, "(?<=[[:space:]][(][12][0-9]{3}[)][;])")) %>%
-    unnest(ref) %>%
-    mutate(
-      ref = str_squish(ref),
-      ref = str_remove(ref, "[;.]$"),
-      doi = str_extract(ref, "\\b10[.][[:digit:]]{4,9}[/][[:print:]]+\\b"))
 }
 
 
 # main --------------------------------------------------------------------
 
-# httr::set_config(httr::user_agent("me@example.com; +https://example.com/info.html"))
-url_root = "https://science.sciencemag.org"
+scimag_bow <- bow(url = "https://science.sciencemag.org",
+                  user_agent = "Joao Martins <https://github.com/zambujo>",
+                  force = TRUE)
 
 archive_links <- 2005:2020 %>%
-  sample(1) %>%
-  map(parse_year_issues) %>%
+  map(parse_year_issues, polite_bow = scimag_bow) %>%
   flatten_chr()
 
-(twil_links <- glue("{archive_links}/twil"))
+sample_result <- glue("{archive_links}/twil") %>%
+  map_df(parse_twil, polite_bow = scimag_bow)
 
-sample_result <- twil_links %>%
-  sample(3) %>%
-  map_df(parse_twil)
+# post-processing ---------------------------------------------------------
 
-sample_result
+sample_result <- sample_result %>%
+  rowid_to_column("id") %>%
+  mutate(ref = str_split(ref, "(?<=[[:space:]][(][12][0-9]{3}[)][;])")) %>%
+  unnest(ref) %>%
+  mutate(
+    ref = str_squish(ref),
+    ref = str_remove(ref, "[;.]$"),
+    doi = str_extract(ref, "\\b10[.][[:digit:]]{4,9}[/][[:graph:]]+\\b")
+  )
