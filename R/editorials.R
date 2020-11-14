4bc62ef6d2c698c3181b82309e7391f8d21848ed
Martins <zambuj@gmail.com>
2020-11-14 21:58:10 +0100

    🍱  Re-scraped sciencemag for editorial highlights

--- R/editorials.R
+++ R/editorials.R
@@ -1,5 +1,8 @@
 library("here")
 source(here("R", "packages.R"))
+
+
+# science magazine --------------------------------------------------------
 source(here("R", "sciencemag.R"))
 
 session_bow <- bow(url = "https://science.sciencemag.org",
@@ -17,18 +20,17 @@ glue("{archive_links}/twil") %>%
        file_path = here("data-raw", "sciencemag-highlights.csv"),
        polite_bow = session_bow)
 
-# post-processing ---------------------------------------------------------
-if (FALSE) {
-  highlights <- highlights %>%
-    rowid_to_column("id") %>%
+
+highlights <- read_csv(here("data-raw", "sciencemag-highlights.csv"))
+
+highlights <- highlights %>%
     mutate(ref = str_split(ref, "(?<=[[:space:]][(][12][0-9]{3}[)][;])")) %>%
     unnest(ref) %>%
     mutate(
       ref = str_squish(ref),
       ref = str_remove(ref, "[;.]$"),
-      doi = str_extract(ref, "\\b10[.][[:digit:]]{4,9}[/][[:graph:]]+\\b")
-    )
+      doi = str_extract(ref, "\\b10[.][[:digit:]]{4,9}[/][[:graph:]]+\\b"))
 
-  write_csv(highlights, here("data", "sciencemag.csv"))
-}
+write_csv(highlights, here("data", "sciencemag.csv"))
+
 
