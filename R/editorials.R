5a5d58c85a85fc4512a5487d6bba6bc7e80e8795
Martins <zambuj@gmail.com>
2020-11-17 12:04:56 +0100

    🍱  Adding nature journal raw data (See #10)

--- R/editorials.R
+++ R/editorials.R
@@ -1,85 +1,73 @@
 library("here")
 source(here("R", "packages.R"))
+source(here("R", "sciencemag.R"))
+source(here("R", "nature.R"))
 
 TIME_PERIOD <- 2005:2020
 
 # science magazine --------------------------------------------------------
-if (FALSE) {
-  source(here("R", "sciencemag.R"))
+session_bow <- bow(url = "https://science.sciencemag.org",
+                   user_agent = "Zambujo <https://github.com/zambujo/editorials>",
+                   force = TRUE)
 
-  session_bow <- bow(url = "https://science.sciencemag.org",
-                     user_agent = "Zambujo <https://github.com/zambujo/editorials>",
-                     force = TRUE)
+# issues
+archive_links <- TIME_PERIOD %>%
+  map(parse_year_issues, polite_bow = session_bow) %>%
+  flatten_chr()
 
-  # get issues
-  archive_links <- TIME_PERIOD %>%
-    map(parse_year_issues, polite_bow = session_bow) %>%
-    flatten_chr()
+# articles
+glue("{archive_links}/twil") %>%
+  walk(
+    parse_twil,
+    file_path = here("data-raw", "sciencemag-highlights.csv"),
+    polite_bow = session_bow
+  )
 
-  # get editorial highlights
-  glue("{archive_links}/twil") %>%
-    walk(
-      parse_twil,
-      file_path = here("data-raw", "sciencemag-update.csv"),
-      polite_bow = session_bow
-    )
+# tidy up
+highlights <-
+  read_csv(here("data-raw", "sciencemag-highlights.csv"))
 
-  highlights <-
-    read_csv(here("data-raw", "sciencemag-highlights.csv"))
-
-  highlights <- highlights %>%
-    mutate(ref = str_split(ref, "(?<=[[:space:]][(][12][0-9]{3}[)][;])")) %>%
-    unnest(ref) %>%
-    mutate(
-      ref = str_squish(ref),
-      ref = str_remove(ref, "[;.]$"),
-      doi = str_extract(ref, "\\b10[.][[:digit:]]{4,9}[/][[:graph:]]+\\b")
-    )
-
-  write_csv(highlights, here("data", "sciencemag.csv"))
-}
+highlights %>%
+  mutate(ref = str_split(ref, "(?<=[[:space:]][(][12][0-9]{3}[)][;])")) %>%
+  unnest(ref) %>%
+  mutate(
+    ref = str_squish(ref),
+    ref = str_remove(ref, "[;.]$"),
+    doi = str_extract(ref, "\\b10[.][[:digit:]]{4,9}[/][[:graph:]]+\\b")
+  ) %>%
+  write_csv(here("data", "sciencemag.csv"))
 
 # nature ------------------------------------------------------------------
-if (FALSE) {
-  source(here("R", "nature.R"))
+session_bow <- bow(url = "https://www.nature.com",
+                   user_agent = "Martins <https://github.com/zambujo>",
+                   force = TRUE)
 
-  session_bow <- bow(url = "https://www.nature.com",
-                     user_agent = "Martins <https://github.com/zambujo>",
-                     force = TRUE)
+# volumes ...
+nature_volumes <- nature_volumes(session_bow) %>%
+  filter(year %in% TIME_PERIOD)
 
-  # volumes
-  nature_volumes <- parse_volumes(session_bow) %>%
-    filter(year %in% TIME_PERIOD)
+# issues ...
+nature_issues <- nature_volumes %>% pull(volume_key) %>%
+  map(nature_issues, polite_bow = session_bow) %>%
+  bind_rows()
 
-  # issues
-  nature_issues <- nature_volumes %>% pull(volume_key) %>%
-    map(parse_volume_issues, polite_bow = session_bow) %>%
-    bind_rows()
+write_csv(nature_issues, here("data-raw", "nature-issues.csv"))
 
-  write_csv(nature_issues, here("data-raw", "nature-issues.csv"))
-}
+# contents ...
+read_csv(here("data-raw", "nature-issues.csv")) %>%
+  pull(issue_key) %>%
+  walk(
+    nature_contents,
+    file_path = here("data-raw", "nature-contents.csv"),
+    polite_bow = session_bow
+  )
 
-if (TRUE) {
-  source(here("R", "nature.R"))
-  session_bow <- bow(url = "https://www.nature.com",
-                     user_agent = "Martins <https://github.com/zambujo>",
-                     force = TRUE)
-
-  read_csv(here("data-raw", "nature-issues.csv")) %>%
-    pull(issue_key) %>%
-    walk(
-      parse_issue_thisweek,
-      file_path = here("data-raw", "nature-contents.csv"),
-      polite_bow = session_bow
-    )
-
-  # highlights
-  read_csv(here("data-raw", "nature-contents.csv")) %>%
-    filter(contents_labels == "Research Highlights") %>%
-    pull(article_key) %>%
-    walk(
-      parse_article,
-      file_path = here("data-raw", "nature-highlights.csv"),
-      polite_bow = session_bow
-    )
-}
+# articles ...
+read_csv(here("data-raw", "nature-contents.csv")) %>%
+  filter(str_detect(contents_labels, "Research Highlight")) %>%
+  pull(article_key) %>%
+  walk(
+    nature_articles,
+    file_path = here("data-raw", "nature-highlights.csv"),
+    polite_bow = session_bow
+  )
--- R/nature.R
+++ R/nature.R
@@ -1,17 +1,35 @@
-parse_volumes <- function(polite_bow) {
-  session <- nod(bow = polite_bow, path = "nature/volumes")
-  volumes <- scrape(session) %>% html_nodes("#volume-decade-list")
+#' Retrieve the URL of the listed volumes of the journal
+#'
+#' @param polite_bow An HTTP polite::bow session object.
+#'
+#' @return A tibble with the URL, the date, and the corresponding
+#' year of publication of each volume.
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' nature_volumes(bow("https://www.nature.com"))
+#' }
+#'
+nature_volumes <- function(polite_bow) {
+  if (missing(polite_bow))
+    stop("Missing polite::bow() session")
+
+  session <- nod(bow = polite_bow,
+                 path = "nature/volumes")
+  volumes <- scrape(session) %>%
+    html_nodes("#volume-decade-list")
 
   tibble(
     volume_key = volumes %>% html_nodes("a") %>% html_attr("href"),
-    date =   volumes %>% html_nodes("time") %>% html_text()
+    date = volumes %>% html_nodes("time") %>% html_text()
   ) %>%
     mutate(year = str_extract(date, "\\d{4}"),
            year = as.integer(year))
 }
 
 
-parse_volume_issues <- function(volume, polite_bow) {
+nature_issues <- function(volume, polite_bow) {
   glue("Parsing {volume} ...") %>% message()
 
   session <- nod(bow = polite_bow, path = volume)
@@ -25,7 +43,7 @@ parse_volume_issues <- function(volume, polite_bow) {
   )
 }
 
-parse_issue_thisweek <- function(issue, file_path, polite_bow) {
+nature_contents <- function(issue, file_path, polite_bow) {
   glue("Parsing {issue} ...") %>% message()
 
   session <- nod(bow = polite_bow, path = issue)
@@ -49,10 +67,16 @@ parse_issue_thisweek <- function(issue, file_path, polite_bow) {
       html_text()
   )
 
-  write_csv(res, file_path, append = file.exists(file_path))
+  if (nrow(res) > 0) {
+    if (missing(file_path)) {
+      return(res)
+    } else {
+      write_csv(res, file_path, append = file.exists(file_path))
+    }
+  }
 }
 
-parse_article <- function(article, file_path, polite_bow) {
+nature_articles <- function(article, file_path, polite_bow) {
   glue("Parsing {article} ...") %>% message()
 
   session <- nod(bow = polite_bow, path = article)
@@ -71,7 +95,6 @@ parse_article <- function(article, file_path, polite_bow) {
 
   doi_txt <- NA_character_
   doi_link <- NA_character_
-
 
   if (str_to_lower(article_title) == "research highlights") {
     # early volumes -----------------------------------------
@@ -138,5 +161,9 @@ parse_article <- function(article, file_path, polite_bow) {
     doi = doi_link
   )
 
-  write_csv(res, file_path, append = file.exists(file_path))
+  if (missing(file_path)) {
+    return(res)
+  } else {
+    write_csv(res, file_path, append = file.exists(file_path))
+  }
 }
